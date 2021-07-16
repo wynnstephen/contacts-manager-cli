dd396f7cae8d8488f0a0b83f819eb801c018cef2
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-16 14:02:03 -0500

    adds this

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,18 +1,48 @@
-import java.util.HashMap;
-
 public class Contacts {
+    private String firstName;
 
-    public static HashMap<String, Contacts> contacts = new HashMap<>();
+    private String lastName;
+    double phoneNumber;
 
-
-
-
-
-    public static void main(String[] args) {
-        System.out.println(contacts);
+    public Contacts(String firstName, String lastName, double phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
     }
-    contacts.put("Jack Blank", "024-234-5678");
-    contacts.put("Jane Doe", "678-456-8907");
-    contacts.put("Sam Space", "334-345-8345");
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(double phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+//    public static HashMap<String, Contacts> contacts = new HashMap<>();
+//
+//
+//    public static void main(String[] args) {
+//        System.out.println(contacts);
+//    }
+//    contacts.put("Jack Blank", "024-234-5678");
+//    contacts.put("Jane Doe", "678-456-8907");
+//    contacts.put("Sam Space", "334-345-8345");
+
 }
 
